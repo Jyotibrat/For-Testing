d49e852d10f585375ee1762d4ade5006a829ec60
Jyotibrat <jyotibratb@gmail.com>
2025-06-12 16:01:39 +0530

    Updated

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -28,7 +28,7 @@ const ContactSection: React.FC = () => {
     setStatus({ loading: true, success: false, error: "" });
 
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch("https://for-testing-self.vercel.app/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
